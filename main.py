8d5ea72cbfd18944a6fcbf25df61c62ff49a413a
brennanh07 <brennanh07@vt.edu>
2024-06-15 13:14:22 -0400

    timer finished

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+import math
 import time
 
 # Constants
@@ -12,34 +13,62 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title.configure(text="Long Break")
+    elif reps % 2 == 8:
+        count_down(short_break_sec)
+        title.configure(text="Short Break")
+    else:
+        count_down(work_sec)
+        title.configure(text="Work Time")
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # UI
 
 window = Tk()
 window.title("Pomodoro")
 window.geometry("500x440")
-window.config(padx=75, pady=50, bg=YELLOW)
+window.config(padx=70, pady=50, bg=YELLOW)
+
 
 
 # Tomato image / timer
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 
 # Start button
-start_button = ttk.Button(text="Start")
+start_button = ttk.Button(text="Start", command=start_timer)
 start_button.grid(column=0, row=2)
 
 # Reset button
